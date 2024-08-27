aa64a66bac920299657c314e1acb351a417e8842
GrzegorzStacel <stacelgrzegorz@gmail.com>
2024-08-27 18:16:56 +0200

    Convert Buffer to the string

--- src/core/fileReader.ts
+++ src/core/fileReader.ts
@@ -5,6 +5,9 @@ export function readInputFile(filename: string): Promise<InputData | null> {
   return new Promise((resolve) => {
     fs.readFile(filename, "utf8", (err, data) => {
       if (err) {
+      // Konwertowanie Buffer na string
+      const fileContent = data.toString();
+
         console.log("error");
         resolve(null);
         return;
