28a56d7dd6e818fa9e781db863eb0073c7b8c284
GrzegorzStacel <stacelgrzegorz@gmail.com>
2024-08-27 18:21:23 +0200

    Add values to the teams arrays

--- src/core/pieceFactory.ts
+++ src/core/pieceFactory.ts
@@ -11,6 +11,10 @@ export function createPieces(boardX: number, speedA: number, speedB: number, boa
     // Obliczanie prędkości dla drużyny B
     const vB = x % 2 === 0 ? 1 * speedB : Math.pow(2, speedB);
 
+    // Drużyna A zaczyna na górze planszy (y = boardY - 1) i schodzi w dół
+    teamAPieces.push({ x, y: boardY - 1, v: vA });
+    // Drużyna B zaczyna na dole planszy (y = 0) i wchodzi w górę
+    teamBPieces.push({ x, y: 0, v: vB });
   }
 
   return { teamAPieces, teamBPieces };
